addc5cc5086c75f0af8976ad4d2e4d5ff2e9a5f8
Siggi <skbirdandsiggi10@gmail.com>
2024-10-24 23:50:17 +0000

    lesgo

--- scripts.js
+++ scripts.js
@@ -7,6 +7,14 @@
  */
 
 import { isString, splitOnWhitespace } from './lib/helpers.js';
+import { reverse } from './lib/reverse.js';
+import { longest } from './lib/longest.js';
+import { shortest } from './lib/shortest.js';
+import { palindrome } from './lib/palindrome.js';
+import { vowels } from './lib/vowels.js';
+import { consonants } from './lib/consonants.js';
+
+
 
 const test = isString('hæ');
 console.log('test er strengur?', test);
@@ -16,3 +24,53 @@ const stringWithWhitespace = `halló
 hæ`;
 const split = splitOnWhitespace(stringWithWhitespace);
 console.log(split);
+
+const formElement = document.querySelector('form')
+
+function submitHandler(event) {
+    event.preventDefault();
+    const { target } = event;
+    const textareaElement = target.querySelector('textarea');
+
+    const inputElement = document.querySelector('.input');
+    inputElement.textContent = textareaElement.value;
+
+    const longestElement = document.querySelector('.longest');
+    longestElement.textContent = longest(textareaElement.value);
+
+    const reversedElement = document.querySelector('.reversed');
+    reversedElement.textContent = reverse(textareaElement.value);
+
+    const shortestElement = document.querySelector('.shortest');
+    shortestElement.textContent = shortest(textareaElement.value);
+
+    const vowelsElement = document.querySelector('.vowels');
+    vowelsElement.textContent = vowels(textareaElement.value);
+
+    const consonantsElement = document.querySelector('.consonants');
+    consonantsElement.textContent = consonants(textareaElement.value);
+
+    const palindromeElement = document.querySelector('.palindrome');
+    const palindromes = palindrome(textareaElement.value);
+
+    if (palindromes == true) {
+        palindromeElement.textContent = '';
+    }
+    else {
+        palindromeElement.textContent = 'ekki';
+    }
+
+    console.log(textareaElement.value);
+} 
+
+formElement.addEventListener('submit', submitHandler)
+
+function removeHidden(el){
+    el.classList.remove('hidden');
+}
+
+const outputElement = document.querySelector('.output')
+
+if (outputElement) {
+    removeHidden(outputElement);
+}
